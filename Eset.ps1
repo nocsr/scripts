f36f908f914bbbb7cd270547bb74f78c4c189d59
nocsr <48022157+nocsr@users.noreply.github.com>
2019-02-27 12:58:58 +0100

    Update Eset.ps1

--- Eset.ps1
+++ Eset.ps1
@@ -1,1 +1,1 @@
-if ((Get-ItemProperty -path 'HKLM:\SOFTWARE\ESET\ESET Security\CurrentVersion\Info').ProductVersion -like "7.*"){ $Exitcode=0} Else { $Exitcode=1}
+if ((Get-ItemProperty -path 'HKLM:\SOFTWARE\ESET\ESET Security\CurrentVersion\Info').ProductVersion -like "7.*"){ Exit 0} Else { Exit 1}

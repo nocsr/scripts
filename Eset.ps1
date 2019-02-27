10d0e2e32456c713b553cfb93a0a920a92d87c89
nocsr <48022157+nocsr@users.noreply.github.com>
2019-02-27 16:35:13 +0100

    Update Eset.ps1

--- Eset.ps1
+++ Eset.ps1
@@ -1,12 +1,11 @@
 $test=Try 
 {
 (Get-ItemProperty -path 'HKLM:\SOFTWARE\ESET\ESET Security\CurrentVersion\Info').ProductVersion -like "7.*"
-Write-host"La clé n'existe pas"
 }
 Catch 
 {$Err=$_}
 If ($test){
 If ((Get-ItemProperty -path 'HKLM:\SOFTWARE\ESET\ESET Security\CurrentVersion\Info').ProductVersion -like "7.*")
-{Exit 0 Write-host "Clé trouvée"} Else {Exit 1}
+{Write-host"La clé existe"} Else {Exit 1}
 }
-Else {Exit 1}
+Else {Write-host"La clé n'existe pas"}

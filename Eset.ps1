408d574a400c5710f8a66ed91b87b58a21dce38b
nocsr <48022157+nocsr@users.noreply.github.com>
2019-02-28 08:42:56 +0100

    Update Eset.ps1

--- Eset.ps1
+++ Eset.ps1
@@ -1,11 +1,8 @@
-$test=Try 
+$Test=Get-WmiObject -Class Win32_Product -Filter "Name like 'Eset%'"
+$Test=Get-WmiObject -Class Win32_Product -Filter "Name like 'Eset%'"
+If ($Test)
 {
-(Get-ItemProperty -path 'HKLM:\SOFTWARE\ESET\ESET Security\CurrentVersion\Info').ProductVersion -like "7.*"
+If((Get-ItemProperty -path 'HKLM:\SOFTWARE\ESET\ESET Security\CurrentVersion\Info').ProductVersion -like "7.*"){Exit 0}
 }
-Catch 
-{$Err=$_}
-If ($test){
-If ((Get-ItemProperty -path 'HKLM:\SOFTWARE\ESET\ESET Security\CurrentVersion\Info').ProductVersion -like "7.*")
-{Write-host "La clé existe"} Else {Exit 1}
-}
-Else {Write-host "La clé n'existe pas"}
+Else
+{Exit 1}

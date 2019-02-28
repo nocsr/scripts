2edf5b10c0a24067d1538c91d766891f15b2860a
nocsr <48022157+nocsr@users.noreply.github.com>
2019-02-28 13:54:53 +0100

    Delete Eset.ps1

--- Eset.ps1
+++
@@ -1,8 +0,0 @@
-$Test=Get-WmiObject -Class Win32_Product -Filter "Name like 'Eset%'"
-$Test=Get-WmiObject -Class Win32_Product -Filter "Name like 'Eset%'"
-If ($Test)
-{
-If((Get-ItemProperty -path 'HKLM:\SOFTWARE\ESET\ESET Security\CurrentVersion\Info').ProductVersion -like "7.*"){Exit 0}
-}
-Else
-{Exit 1}
